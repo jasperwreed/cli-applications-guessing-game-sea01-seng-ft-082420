3b2ef424fa02af87ed50eb6ff9d02014050d4aa6
Jasper Reed <jmixon.reed@gmail.com>
2020-08-12 01:37:58 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game
   rand_num = rand(1..6)
   
   puts "Guess number between 1 and 6"
-  user_input = gets
+  user_input = gets.chomp
   
   if user_input == rand_num
     puts "You guessed the correct number!"
