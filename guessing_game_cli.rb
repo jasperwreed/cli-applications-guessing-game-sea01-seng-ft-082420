7f117cf30a95c7b03f6ca5f9ad246456ff44567f
Jasper Reed <jmixon.reed@gmail.com>
2020-08-12 01:35:47 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,4 +2,16 @@ require 'pry'
 
 def run_guessing_game
   rand_num = rand(1..6)
+  
+  puts "Guess number between 1 and 6"
+  user_input = gets
+  
+  if user_input == rand_num
+    puts "You guessed the correct number!"
+  elsif !user_input == rand_num
+    puts "Sorry! The computer guessed #{rand_num}."
+  elsif user_input == "exit"
+    puts "Goodbye!"
+  end
+  
 end
