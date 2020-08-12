f07ccd13529691900cadfdfd17afa128194a863d
Jasper Reed <jmixon.reed@gmail.com>
2020-08-12 01:44:38 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,9 @@ def run_guessing_game
   puts "Guess number between 1 and 6"
   user_input = gets.chomp
   
-  if !user_input == rand_num
+  if user_input == rand_num.to_s
+    puts "You guessed the correct number!"
+  elsif !user_input == rand_num.to_s
     puts "Sorry! The computer guessed #{rand_num}."
   elsif user_input == "exit"
     puts "Goodbye!"
